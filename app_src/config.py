784eba75950eaf49ce915ec9da3fd8e8a6ff080a
Lucile Lévêque <lululeveq@hotmail.fr>
2020-11-15 16:09:18 +0100

    Config: docstrings added

--- app_src/config.py
+++ app_src/config.py
@@ -1,4 +1,8 @@
 import os
 
 class Config(object):
+    """
+        Classe Config permettant de définir toutes les variables de configuration
+        nécessaire à l'application Flask.
+    """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' # TODO: A changer quand lancement en prod'
