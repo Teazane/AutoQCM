a77f1df7c19f1db804896cf2a7b35837c721b23c
Lucile Lévêque <lululeveq@hotmail.fr>
2020-12-28 00:08:35 +0100

    data_loader: classe ajoutée pour les tables de correspondance

--- data_management/data_loader.py
+++ data_management/data_loader.py
@@ -53,10 +53,81 @@ class QuestionLoader:
         if subject is not None:
             data_to_filter = data_to_filter[data_to_filter["Num-Discipline"] == subject]
         return data_to_filter
-        
+
+class CorrespondingTablesLoader:
+    """
+        Classe CorrespondingTablesLoader permettant de charger les tables de correspondance 
+        de la BDD depuis un fichier Excel ou CSV au besoin.
+    """
+
+    def __init__(self, bdd_file_path):
+        """
+            Constructeur de la classe CorrespondingTablesLoader.
+            Initialise le logger et charge les données du fichier passé en paramètre.
+
+            :param bdd_file_path: Chemin du fichier xlsx à lire.
+            :type bdd_file_path: string
+        """
+        logging.basicConfig(format='%(asctime)s %(levelname)s [%(module)s (%(funcName)s)] %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.DEBUG)
+        logging.info("Data base file reading...")
+        self.dataframe_level_from_xlsx = read_excel(bdd_file_path, sheet_name="Num-Niveau")
+        self.dataframe_difficulty_from_xlsx = read_excel(bdd_file_path, sheet_name="Num-Difficulté")
+        self.dataframe_notion_from_xlsx = read_excel(bdd_file_path, sheet_name="Num-Rudiment")
+        self.dataframe_subject_from_xlsx = read_excel(bdd_file_path, sheet_name="Num-Discipline")
+
+    def get_corresponding_level(self, level):
+        """
+            Méthode pour trouver le nom d'un niveau scolaire à partir de son ID.
+
+            :param level: ID du niveau scolaire cherché.
+            :type level: int
+            :return: Nom du niveau scolaire. 
+            :rtype: string
+        """
+        res = self.dataframe_level_from_xlsx["Niveau"][self.dataframe_level_from_xlsx["Num-Niveau"] == level]
+        return res.iloc[0]
+
+    def get_corresponding_difficulty(self, difficulty):
+        """
+            Méthode pour trouver le nom d'un niveau de difficulté à partir de son ID.
+
+            :param difficulty: ID du niveau de difficulté cherché.
+            :type difficulty: int
+            :return: Nom du niveau de difficulté. 
+            :rtype: string
+        """
+        res = self.dataframe_difficulty_from_xlsx["Difficulté"][self.dataframe_difficulty_from_xlsx["Num-Difficulté"] == difficulty]
+        return res.iloc[0]
+
+    def get_corresponding_notion(self, notion):
+        """
+            Méthode pour trouver le nom d'une notion à partir de son ID.
+
+            :param notion: ID d'une notion cherché.
+            :type notion: int
+            :return: Nom d'une notion. 
+            :rtype: string
+        """
+        res = self.dataframe_notion_from_xlsx["Rudiment"][self.dataframe_notion_from_xlsx["Num-Rudiment"] == notion]
+        return res.iloc[0]
+
+    def get_corresponding_subject(self, subject):
+        """
+            Méthode pour trouver le nom d'une matière scolaire à partir de son ID.
+
+            :param subject: ID d'une matière scolaire cherchée.
+            :type subject: int
+            :return: Nom d'une matière scolaire. 
+            :rtype: string
+        """
+        res = self.dataframe_subject_from_xlsx["Discipline"][self.dataframe_subject_from_xlsx["Num-Discipline"] == subject]
+        return res.iloc[0]
 
 if __name__ == '__main__':
     # Main used for test
     ql = QuestionLoader("BDD-Questions.xlsx")
     print(ql.get_all_questions_with_filter(level=9))
     print(ql.get_all_questions_with_filter(level=9, difficulty=4))
+    ctl = CorrespondingTablesLoader("BDD-Questions.xlsx")
+    print(ctl.get_corresponding_level(7))
+    print(ctl.get_corresponding_notion(1))
