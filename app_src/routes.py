b23e1be6ee10d436d51e921af033f55ec65e9c70
Lucile Lévêque <lululeveq@hotmail.fr>
2021-05-05 22:55:04 +0200

    Traitement du formulaire dans l'HTML et routes.py

--- app_src/routes.py
+++ app_src/routes.py
@@ -1,12 +1,21 @@
 from App import app
-from flask import render_template
+from flask import render_template, redirect
 from jinja2 import Environment, PackageLoader
+from .forms.questionnaire_form import Generate_questionnaire
 env_jinja = Environment(loader=PackageLoader('app_src', 'templates'))
 
-@app.route('/')
-@app.route('/questionnaire')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/questionnaire', methods=['GET', 'POST'])
 def questionnaire():
-    return render_template(env_jinja.get_template('questionnaire.html'), title='Accueil')
+    form = Generate_questionnaire()
+    form.subject.choices = [(0, "Choix 0"), (1, "Choix 1")]
+    form.notion.choices = [(0, "Choix 0"), (1, "Choix 1")]
+    form.level.choices = [(0, "Choix 0"), (1, "Choix 1")]
+    form.difficulty.choices = [(0, "Choix 0"), (1, "Choix 1")]
+    if form.validate_on_submit():
+        # TODO
+        redirect("/")
+    return render_template(env_jinja.get_template('questionnaire.html'), form=form)
 
 @app.route('/favico.ico')
 def favicon():
