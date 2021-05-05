ab61dcff59677f2af109a10ac56f75a50f0b1dcd
Lucile Lévêque <lululeveq@hotmail.fr>
2021-05-05 23:45:56 +0200

    Division en deux étapes (donc fomulaires) de la génération du questionnaire

--- app_src/forms/questionnaire_form.py
+++ app_src/forms/questionnaire_form.py
@@ -3,10 +3,13 @@ from wtforms import FileField, SelectMultipleField, IntegerField, SelectField, S
 from wtforms.validators import DataRequired
 
 class Generate_questionnaire(FlaskForm):
-    bdd_file = FileField(label="Base de questions", validators=[DataRequired()])
     subject = SelectMultipleField(label="Discipline(s)", coerce=int)
     notion = SelectMultipleField(label="Rudiment(s)", coerce=int)
     level = SelectMultipleField(label="Niveau(x) scolaire(s)", coerce=int)
     time = IntegerField(label="Temps de l'épreuve")
     difficulty = SelectField(label="Difficulté de l'épreuve", coerce=int)
     submit = SubmitField(label="Générer le questionnaire")
+
+class DB_choice(FlaskForm):
+    db_file = FileField(label="Base de questions", validators=[DataRequired()])
+    submit = SubmitField(label="Charger la base de questions")
